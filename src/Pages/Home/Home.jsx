e4cbb65b8867b4ba3f6f2e24f35fd6a52e2066fb
MD Ariful Islam Shanto <arifulislam1077@gmail.com>
2024-04-02 01:27:33 +0600

    Set up react router, font and favicon for the page

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-white text-9xl'>This is home page</h1>
+        </div>
+    );
+};
+
+export default Home;
